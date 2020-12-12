3eea4059e90192d8d83285e782be25732c811fdc
StoyanKostadinov97 <stoyanKostadinov@outlook.com>
2020-12-12 15:07:13 +0200

    -

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 
 const { mongoose } = require('./db/mongoose');
 const BudgetItem = require('./db/model/budget-item');
+const LinkItem = require('./db/model/link-item');
 const ShoppingItem = require('./db/model/shop-item');
 const User = require('./db/model/user-model');
 const CalendarTask = require('./db/model/calendar-task');
@@ -11,41 +12,38 @@ const jwt = require('./jwt');
 const auth = require('./auth');
 
 
-
-
 const app = express();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
 app.use(express.json());
-
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 
 
 app.get('/', function (req, res) {
   res.send("Hello from server")
 })
 
+
 /**
  * 
  * Calendar
  * 
  */
-  
+
 app.post('/api/task', auth, function (req, res, next) {
   const title = req.body.title;
   const from = req.body.from;
   const to = req.body.to;
   const date = req.body.date;
-  const description=req.body.description;
+  const description = req.body.description;
   const _userId = req.user._id;
 
 
@@ -56,7 +54,7 @@ app.post('/api/task', auth, function (req, res, next) {
     });
 });
 
-app.get('/api/task', auth,function (req, res, next) {
+app.get('/api/task', auth, function (req, res, next) {
   const _userId = req.user._id;
   CalendarTask.find({ _userId }).then((items) => {
     res.send(items);
@@ -66,6 +64,9 @@ app.get('/api/task', auth,function (req, res, next) {
     })
 
 });
+
+
+
 
 /**
  * 
@@ -88,7 +89,7 @@ app.post('/api/budgets', auth, function (req, res, next) {
   const price = req.body.price;
   const from = req.body.from;
   const about = req.body.about;
-  const date = req.body.date;
+  const date = new Date(req.body.date);
   const _userId = req.user._id;
 
 
@@ -112,15 +113,93 @@ app.delete('/api/budgets/:id', function (req, res) {
     })
 });
 
+
+
+/**
+ * 
+ * Links GET & POST & DELETE
+ * 
+ */
+
+app.get('/api/links', auth, function (req, res, next) {
+  const _userId = req.user._id;
+  LinkItem.find({ _userId }).then((items) => {
+    res
+      .status(200)
+      .send(items);
+  })
+    .catch(e => {
+      console.log(e);
+    })
+});
+
+app.post('/api/links', auth, function (req, res, next) {
+  const title = req.body.title;
+  const views = req.body.views;
+  const description = req.body.description === undefined
+    ? ''
+    : req.body.description;
+  const link = req.body.link;
+  const _userId = req.user._id;
+
+
+  return LinkItem
+    .create({ title, views, link, description, _userId })
+    .catch(e => {
+      console.log(e);
+    });
+});
+
+app.delete('/api/links/:id', function (req, res) {
+  LinkItem
+    .findByIdAndDelete({
+      _id: req.params.id
+    })
+    .then((deleted) => {
+      res.send(deleted);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+});
+
+app.patch('/api/links/:id',auth,function(req,res,next){
+  const id=req.params.id;
+  LinkItem
+  .findById({_id:id})
+  .then((finded)=>{
+    finded.views++;
+    console.log(finded);
+    finded.save();
+  })
+  .catch((e)=>{
+    console.log(e);
+  })
+});
+
+
+
 /**
  *
  * Shopping Items  
  *
  */
 
+//   GET ALL SHOPPING ITEMS
+// app.get('/api/shopping',function (req, res, next) {
+// 
+//   ShoppingItem.find({ }).then((items) => {
+//     res.send(items);
+//   })
+//     .catch(e => {
+//       console.log(e);
+//     })
+
+// });
+
 app.get('/api/shopping', auth, function (req, res, next) {
   const _userId = req.user._id;
-  ShoppingItem.find({_userId }).then((items) => {
+  ShoppingItem.find({ _userId }).then((items) => {
     res.send(items);
   })
     .catch(e => {
@@ -142,10 +221,25 @@ app.post('/api/shopping', auth, function (req, res, next) {
     });
 });
 
+app.delete('/api/shopping/:id', function (req, res) {
+  ShoppingItem
+    .findByIdAndDelete({
+      _id: req.params.id
+    })
+    .then((deleted) => {
+      res.send(deleted);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+});
+
+
+
+
 /**
  * User login & register
  */
-
 
 app.post('/api/register', function (req, res, next) {
 
@@ -162,7 +256,10 @@ app.post('/api/register', function (req, res, next) {
 
       return User
         .create({ username, email, password })
-        .then(()=>res.status(200))
+        .then(() => {
+          res.status(200)
+          next();
+        })
         .catch((e) => console.log(e))
     })
 
@@ -193,13 +290,11 @@ app.post('/api/login', function (req, res, next) {
 
       res
         .status(200)
-        .cookie("x-token", token, { maxAge: 360000, httpOnly: true })
+        .cookie("x-token", token, { maxAge: 1800000, httpOnly: true })
         .send(user);
 
     })
-    .catch((e) => {
-      console.log(e);
-    })
+    .catch(next)
 
 });
 
@@ -217,7 +312,7 @@ app.get('/api/logout', function (req, res, next) {
  * 
  */
 
- 
+
 //Only for testing
 app.get('/api/user', function (req, res, next) {
   User.find({}).then((result) => {
